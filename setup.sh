adc3d042cbeef03e634d57a62280f7ae7dc61623
Steven McDonald <stmc6816@colorado.edu>
2020-10-31 15:08:07 -0400

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+# mit-deeplearning setup script
+set -euxo pipefail
+
+pip3 install --upgrade "six" "cv2" "ipython" "imageio" "imageio-ffmpeg" "matplotlib" "seaborn" "numpy" "pandas" "tensorflow" "keras" "tabulate" "pil" "tqdm" "scipy" "tensorflow-hub" 
